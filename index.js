863f6672e2c6b7e48d908b5290283ddc149f76dd
Chase Wex <clapingmanly@hotmail.com>
2019-06-25 19:47:08 -0400

    Updated endpoints to match features in the model

--- index.js
+++ index.js
@@ -20,23 +20,40 @@ server.listen(port, function() {
 });
 
 
+server.get("/api/stats",
+  (req,res) => { db_words.getStats(req.headers.token ? req.headers.token : null)
+  .then(result => res.status(200).json(result))
+  //.catch(err => res.status(400).json({error: err, message: "Could not gather from database"}))
+  });
+
 server.get("/api/words",
   (req,res) => { db_words.getNewWord()
   .then(result => res.status(200).json(result))
-  .catch(err => res.status(400).json({error: err, message: "could not gather from database"}))
+  .catch(err => res.status(400).json({error: err, message: "Could not gather from database"}))
+  });
+server.get("/api/words/:id",
+  (req,res) => { db_words.findWord(req.params.id)
+  .then(result => res.status(200).json(result))
+  .catch(err => res.status(400).json({error: err, message: "Could not gather from database"}))
+  });
+
+  server.post("/api/words",
+  (req,res) => { db_words.addStats(req.body, req.headers.token ? req.headers.token : null)
+  .then(result => res.status(200).json(result))
+  //.catch(err => res.status(400).json({error: err, message: "Could not gather from database"}))
   });
 
 server.get("/api/users/:id",
   (req,res) => { db_auth.find(req.headers.token, req.params.id)
   .then(result => res.status(200).json(result))
-  .catch(err => res.status(400).json({error: err, message: "could not gather from database"}))}
+  .catch(err => res.status(400).json({error: err, message: "Could not gather from database"}))}
 );
 
-server.get("/api/users/",
+server.get("/api/users",
 (req,res) => 
 db_auth.find(req.headers.token)
 .then(result => res.status(200).json(result))
-.catch(err => res.status(400).json({error: err, message: "could not gather from database"}))
+.catch(err => res.status(400).json({error: err, message: "Could not gather from database"}))
 );
 
 server.post("/api/register",
@@ -45,12 +62,12 @@ server.post("/api/register",
   db_auth.userExists(req.body.username).then(() => 
   {
     if(!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.exec(req.body.password))
-      return res.status(400).json({error: "bad password", message: "password must contain 8 characters and have one upper, one lower, and one number"});
+      return res.status(400).json({error: "bad password", message: "Password must contain 8 characters and have one upper, one lower, and one number"});
     db_auth.register(req.body.username, req.body.password)
         .then(result => res.status(201).json(result))
-        .catch(err => res.status(500).json({error: err, message: "interal error"}))
+        .catch(err => res.status(500).json({error: err, message: "Interal error"}))
   }
-  ).catch(err => res.status(400).json({error: err, message: "username is already in use"}))
+  ).catch(err => res.status(400).json({error: err, message: "Username is already in use"}))
   }
 );
 
@@ -60,14 +77,8 @@ server.post("/api/login",
   .then(result => {
     res.status(200).json(result)
   })
-  .catch(err => res.status(400).json({error: err, message: "incorrect username or password"}))
+  .catch(err => res.status(400).json({error: err, message: "Incorrect username or password"}))
 );
-
-server.get("/api/logout", (req,res) =>
-{
-  res.send("logout")
-});
-
 
 function logger(req,res,next)
   {
