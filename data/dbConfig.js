bb04cb10e1e83d2c27882b17a96b249e4e09ee0b
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2019-12-17 21:33:23 -0800

    Update connection info

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 
-const configOptions = require('../knexfile')[process.env.DB_ENV];
+const configOptions = require('../knexfile')[process.env.NODE_ENV];
 console.log(configOptions);
 
 module.exports = knex(configOptions);
