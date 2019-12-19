4aa8ee55375907da30a8c1b13f14215fa94dc66f
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2019-12-19 10:18:03 -0800

    Database hotfix

--- knexfile.js
+++ knexfile.js
@@ -2,11 +2,15 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: 'ec2-54-221-214-3.compute-1.amazonaws.com',
+      // host: 'ec2-54-221-214-3.compute-1.amazonaws.com',
+      host: process.env.HOST,
       port: 5432,
-      user: 'oerbziclxtwpxt',
-      password: '658a1230ea308d76f935c6ea47dabbabbb668c48f12b84db5b19dc1de7e390de',
-      database: 'd6qfbdc55m8h1k',
+      // user: 'oerbziclxtwpxt',
+      user: process.env.USER,
+      // password: '658a1230ea308d76f935c6ea47dabbabbb668c48f12b84db5b19dc1de7e390de',
+      password: process.env.PASSWORD,
+      // database: 'd6qfbdc55m8h1k',
+      database: process.env.DATABASE,
       ssl: true
     },
     migrations: {
